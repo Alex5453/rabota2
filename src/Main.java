4a88f70382f7c0fb3109a71bc53463b00990cf40
Alex <yanu.z@mail.ru>
2024-01-06 17:53:30 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Здравсвуйте !");
+        int amount = 13_676;
+        int bonus = amount / 20;
+
+        System.out.println("Ваш бонус: " + bonus );
+        System.out.println("До свидания !");
+
+
+
+    }
+}
